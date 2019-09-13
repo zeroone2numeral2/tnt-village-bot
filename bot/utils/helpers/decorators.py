3d22eba7abeb8320f112df56cbaa17973c7b2d46
zeroone2numeral2 <numeralzeroone@gmail.com>
2019-09-13 10:08:37 +0200

    Rimosse funzioni inutili

--- bot/utils/helpers/decorators.py
+++ bot/utils/helpers/decorators.py
@@ -38,29 +38,3 @@ def failwithmessage(func):
                 update.message.reply_html(text)
 
     return wrapped
-
-
-def restricted(func):
-    @wraps(func)
-    def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
-        user_id = update.effective_user.id
-        if config.telegram.admins_only and user_id not in config.telegram.admins:
-            logger.info('unauthorized access denied for %d', user_id)
-            update.message.reply_text('You are not allowed to use this bot')
-            return
-        return func(update, context, *args, **kwargs)
-
-    return wrapped
-
-
-def adminsonly(func):
-    @wraps(func)
-    def wrapped(update: Update, context: CallbackContext, *args, **kwargs):
-        user_id = update.effective_user.id
-        if user_id not in config.telegram.admins:
-            logger.warning('user %d tried to use a restriced command', user_id)
-            update.message.reply_text('You are not allowed to use this command')
-            return
-        return func(update, context, *args, **kwargs)
-
-    return wrapped
